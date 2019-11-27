57748d4065d4d5d9e1b03e41b9e69154f208db5e
Pimenov Roman <proxy_none@mail.ru>
2019-11-27 19:18:51 +1000

    init version 0.1

--- mac_changer.py
+++ mac_changer.py
@@ -13,30 +13,21 @@ def get_arguments():
         parser.error('[-] Please specify an interface, use -- help for more info.')
     elif not options.new_mac:
         parser.error('[-] Please specify a new mac, use --help for more info.')
-        return options
-
-
-def create_file(name):
-    with open(name, 'w') as f:
-        f.write('memasik')
+    return options
 
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + "to " + new_mac)
     subprocess.call(['ifconfig', interface, 'down'])
-    subprocess.call(['ifconfig', interface, 'hr', new_mac])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
     subprocess.call(['ifconfig', interface, 'up'])
 
 
-def main():
-    options = get_arguments()
-    change_mac(options.interface, options.new_mac)
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
 
+# options = get_arguments()
+# change_mac(options.interface, options.new_mac)
 
-if __name__ == '__main__':
-    main()
-    # options = get_arguments()
-    # change_mac(options.interface, options.new_mac)
-
-    # ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
-    # print(ifconfig_result)
+# ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
+# print(ifconfig_result)
